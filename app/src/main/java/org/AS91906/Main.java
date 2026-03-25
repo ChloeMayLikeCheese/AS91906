599d713ccbda92be1c1c637e16991b653ab34a7f
ChloeMayLikeCheese <cheeseidontlike4@gmail.com>
2026-03-26 12:33:56 +1300

    set up input

--- app/src/main/java/org/AS91906/Main.java
+++ app/src/main/java/org/AS91906/Main.java
@@ -1,8 +1,39 @@
 package org.AS91906;
 
+import java.io.IOException;
+
+import org.jline.keymap.BindingReader;
+import org.jline.keymap.KeyMap;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    enum Operation {
+        TEST
     }
-    
+
+    public static void main(String[] args) throws IOException {
+        
+        while (true) {
+            try (Terminal terminal = TerminalBuilder.builder()
+                    .name("KawaiiBank Teller")
+                    .jansi(true) 
+                    .build()) {
+                terminal.enterRawMode(); 
+
+                BindingReader bindingReader = new BindingReader(terminal.reader());
+                KeyMap<Operation> keyMap = new KeyMap<>();
+                keyMap.bind(Operation.TEST, "a");
+
+                Operation op = bindingReader.readBinding(keyMap, null, false);
+                if (op != null) {
+                    switch (op) {
+                        case TEST -> {
+                            System.out.println("Hello World");
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
